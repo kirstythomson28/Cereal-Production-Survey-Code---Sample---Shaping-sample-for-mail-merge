1525e2ed9c19b470c641ca8ae7d78f51c80f04b9
Thomson K (Kirsty) <Kirsty.Thomson@gov.scot>
2025-10-22 15:49:13 +0100

    Fixing export error

--- Cereal Production and Disposal Survey - 2024-25 - Disposals - Code - Sample - Sample for mail merge.R
+++ Cereal Production and Disposal Survey - 2024-25 - Disposals - Code - Sample - Sample for mail merge.R
@@ -4,37 +4,26 @@ library(tidyr)
 library(stringr)
 library(writexl)
 
-# Read in main sample received from census team (test farms excluded)
+#Read in main sample recieved from census team with test farms excluded
 full_data <- read_csv("Main_sample_2025 - Excludes test farms.csv")
 
-# Prepare November data: extract BRN, email, and formatted location code
-nov_data <- full_data %>%
-  select(brn, primary_email, parish, holding) %>%
-  mutate(
-    location = str_c(
-      str_pad(as.character(parish), 3, pad = "0"),
-      str_pad(as.character(holding), 4, pad = "0"),
-      sep = "/"
-    )
-  ) %>%
-  filter(!is.na(location) & location != "")  # Remove missing/blank locations
 
-# Reshape data for mail merge: one row per BRN/email, locations spread across columns
-mail_merge <- nov_data %>%
-  group_by(brn, primary_email) %>%
-  arrange(location, .by_group = TRUE) %>%
-  mutate(location_num = row_number()) %>%
-  pivot_wider(
-    names_from = location_num,
-    values_from = location,
-    names_prefix = "location"
-  ) %>%
+#filter november spreadsheet to just brn, email and location codes 
+nov_data <- full_data %>% 
+  select(brn, primary_email, parish, holding) %>%
+  mutate( parish = str_pad(as.character(parish), width = 3, pad = "0"), 
+          holding = str_pad(as.character(holding), width = 4, pad = "0"), 
+          location = paste(parish, holding, sep = "/") ) %>%
+  select(-parish, -holding) %>% 
+  filter(!is.na(location) & location != "") # removes missing/blank locations
+
+#transform data into long format 
+mail_merge <- nov_data %>% 
+  group_by(brn, primary_email) %>% 
+  arrange(location, .by_group = TRUE) %>% 
+  mutate(location_num = row_number()) %>% 
+  pivot_wider( names_from = location_num, values_from = location, names_prefix = "location" ) %>% 
   ungroup()
 
-# Export spreadsheet ready for mail merge
+#export spreadsheet ready for the mailmerge 
 write_xlsx(mail_merge, "2025-26 - Production - Materials - Sample - November sample for mailmerge excluding friendly farm.xlsx")
-
-
-
-
-
